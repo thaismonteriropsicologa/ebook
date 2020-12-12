e332012ce5ffa4780b258094171fd1f4d13e1cd1
lyncas.david <david.s@malwee.com.br>
2020-12-12 19:55:02 -0300

    try solve error sending email

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -7,19 +7,19 @@ export default async (req, res) => {
     if(req.method === 'POST') {
       const { email } = req.body;
 
-      const dir = path.join(getConfig().serverRuntimeConfig.PROJECT_ROOT, '/')
-      console.log(dir)
-      fs.readdir(__dirname + 'vercel/workpath0/', (err, files) => {
+      // const dir = path.join(getConfig().serverRuntimeConfig.PROJECT_ROOT, '/')
+      // console.log(dir)
+      fs.readdir(__dirname, (err, files) => {
         files.forEach(file => {
           console.log(file);
         });
       });
         
-      fs.readFile(dir , async function (err,data){
-          console.log(data)
-          //await sendEmail({ email, data: data.toString('base64') });
-      });
-      return res.status(200).send(dir);
+      // fs.readFile(dir , async function (err,data){
+      //     //console.log(data)
+      //     //await sendEmail({ email, data: data.toString('base64') });
+      // });
+      return res.status(200).send();
     }
     return res.status(404).json({
         error: {
